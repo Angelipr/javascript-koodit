52b37b217dfac225de560f964b3a7d2ade0540fd
angel <angelipr@metropolia.fi>
2024-11-05 10:45:15 +0200

    moi

---
+++ Moduuli 1/exampe.js
@@ -0,0 +1,13 @@
+const lista =["moi", "öö", "poi"]
+
+const objekti = {avain:"kääk"}
+
+const nimi =prompt("Anna nimi")
+
+
+console.log("Hei maailma, mä oon väsy")
+console.log(objekti)
+console.log(lista)
+
+
+document.querySelector("#nimi").innerHTML = nimi
